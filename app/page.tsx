493acc86ca1cd857de4305af5b0b0962c5a0c7a1
joesanthoshalgonquin <sant0232@algonquinlive.com>
2024-04-04 11:43:16 -0400

    Added a reusable citypicker component and installed some libraries for it

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 
 import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
+import CityPicker from "@/components/CityPicker";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
@@ -13,7 +15,9 @@ export default function Home() {
           Using Next.js 13.3, Tailwind CSS, OpenAI, Tremor2.0 + more
         </Subtitle>
         <Divider className="my-10 bg-black" />
-        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]"></Card>
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+          <CityPicker />
+        </Card>
       </Card>
     </div>
   );
